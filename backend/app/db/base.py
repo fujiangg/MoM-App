8dd59b28ccca6fa47ed487d9d3a6c48532b80652
fujiangg <anggraenifuji10@gmail.com>
2026-04-01 09:46:25 +0700

    update porstgre local and create first user

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,4 +1,5 @@
-from typing import Any
-from app.db.base import Base
-from app.models.user import User
-from app.models.mom import MoM
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
